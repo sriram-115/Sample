e97516e49f513fd68b1152812317fd958cce5558
RajaNamburi <rajanamburi069@gmail.com>
2021-05-29 14:42:45 +0530

    Added New file

---
+++ java_programs/src/java_programs/Example1.java
@@ -0,0 +1,10 @@
+package java_programs;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello sriram charan");
+	}
+
+}
